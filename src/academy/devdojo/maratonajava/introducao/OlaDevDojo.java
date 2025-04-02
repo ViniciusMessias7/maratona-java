c55b2df06d08b082c91a7b6eb1578018bf0e3d68
Lamont <viniciusmessias52@gmail.com>
2025-04-02 14:21:09 -0300

    Initial commit

---
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Isso é um comentário <b>javadoc</b>
+ */
+public class OlaDevDojo {
+    public static void main(String[] args) {
+        //Isso é um comentário de uma linha
+        /*
+            Isso é um comentário
+            de múltiplas
+            linhas
+         */
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
